240a8bc748dfbb68960e0663e4487d51f89d0754
Ankan005 <Ankan005@gmail.com>
2019-12-24 10:44:25 +0000

    Enable Smooth Streaming for secure video playback

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,6 +132,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable_pframes=1 \
     vendor.vidc.enc.disable.pq=true \
     vendor.vidc.enc.narrow.searchrange=1
+    vendor.video.disable.ubwc=1 \
+    mm.enable.sec.smoothstreaming=true
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
